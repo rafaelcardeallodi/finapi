88ad1387735b7e8f5302aea1d477b4d246614b8b
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2021-09-30 19:16:23 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next){
+  const { cpf } = req.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer){
+   return res.status(400).json({ error: "Customer not found!" });
+  }
+  
+  req.customer = customer;
+  
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
@@ -26,16 +41,18 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement', (req, res) => {
- const { cpf } = req.headers;
-
- const customer = customers.find(customer => customer.cpf === cpf);
-
- if(!customer){
-  return res.status(400).json({ error: "Customer not found!" });
- }
-
+// 1ª forma de utilizar middleware na rota
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+ const { customer } = req;
+ 
  return res.json(customer.statement);
-});
+}); 
+
+/**
+ * 2ª forma de utilizar middleware nas rotas
+ * quando precisar que todas as rotas a seguir contenham esse middleware
+ * 
+ * app.use(verifyIfExistsAccountCPF);
+ */
 
 app.listen(3333);
